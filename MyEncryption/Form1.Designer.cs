8baa9cec49dbfb90982fc36c4d36792eb3882847
JoySky <joysky@outlook.com>
2015-05-19 16:12:03 +0800

    获得文本文件中的汉字
    帮助老婆写的程序

---
+++ MyEncryption/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace MyEncryption
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnBuild = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.listBoxNew = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnOpen.Location = new System.Drawing.Point(12, 382);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "打开";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnBuild
+            // 
+            this.btnBuild.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuild.Location = new System.Drawing.Point(93, 382);
+            this.btnBuild.Name = "btnBuild";
+            this.btnBuild.Size = new System.Drawing.Size(75, 23);
+            this.btnBuild.TabIndex = 1;
+            this.btnBuild.Text = "生成";
+            this.btnBuild.UseVisualStyleBackColor = true;
+            this.btnBuild.Click += new System.EventHandler(this.btnBuild_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "文本文件（*.txt）|*.txt";
+            // 
+            // listBoxNew
+            // 
+            this.listBoxNew.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxNew.FormattingEnabled = true;
+            this.listBoxNew.ItemHeight = 12;
+            this.listBoxNew.Location = new System.Drawing.Point(12, 12);
+            this.listBoxNew.Name = "listBoxNew";
+            this.listBoxNew.Size = new System.Drawing.Size(238, 364);
+            this.listBoxNew.TabIndex = 3;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(175, 383);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 413);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.listBoxNew);
+            this.Controls.Add(this.btnBuild);
+            this.Controls.Add(this.btnOpen);
+            this.MinimumSize = new System.Drawing.Size(277, 451);
+            this.Name = "frmMain";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnBuild;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ListBox listBoxNew;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ MyEncryption/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MyEncryption
+{
+    
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        public static List<string> lstString;
+        Regex reg = new Regex(@"[\u4e00-\u9fa5]");//正则表达式判断是不是汉字
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName==string.Empty)
+            {
+                return;
+            }
+            lstString = new List<string>();
+            string path = openFileDialog1.FileName;
+            StreamReader objStreamReader = new StreamReader(path,Encoding.Default);
+            string strTemp = objStreamReader.ReadToEnd();
+            for (int i = 0; i < strTemp.Length;i++ )
+            {
+                string hans = strTemp[i].ToString();
+                if (!reg.IsMatch(hans))
+                {
+                    continue;
+                }
+                else
+                {
+                    lstString.Add(hans);
+                    listBoxNew.Items.Add(hans);
+                }
+            }
+            objStreamReader.Close();
+
+            
+        }
+
+        private void btnBuild_Click(object sender, EventArgs e)
+        {
+            if (listBoxNew.Items.Count==0)
+            {
+                MessageBox.Show("不能生成空文件！","警告");
+                return;
+            }
+            saveFileDialog1.ShowDialog();
+            string path=saveFileDialog1.FileName;
+            if (path==string.Empty)
+            {
+                return;
+            }
+            StreamWriter objStreamWriter = new StreamWriter(path, true);
+            for (int i = 0; i < lstString.Count;i++ )
+            {
+                objStreamWriter.WriteLine(lstString[i]);
+            }
+            objStreamWriter.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listBoxNew.Items.Clear();
+        }
+    }
+}
